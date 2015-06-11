6a02e6b457c2922edfb00d7b93a5a960bbf6ed78
Yatin Sarbalia <yatinsns@gmail.com>
2015-06-11 22:51:46 +0530

    Fix create_post script to take markdown file as parameter only

--- bin/create_post.rb
+++ bin/create_post.rb
@@ -17,17 +17,25 @@ def filename title
   "#{POSTS_DIR}/#{new_filename.chomp}"
 end
 
+def exit_with_error text
+  puts "Error: #{text}"
+  exit 1
+end
+
 def main
-  if POSTS_DIR.nil?
-    puts "Error: Environment variable 'POSTS_DIR' missing"
-    exit 1
-  end
+  exit_with_error "Environment variable 'POSTS_DIR' missing" if POSTS_DIR.nil?
 
   source_filename = ARGV.first
-  title = ARGV.drop(1).join(' ')
-  new_filename = filename title
+  exit_with_error "Markdown filename missing.\nUsage: create_post.rb <filename.markdown>" if source_filename.nil?
+  exit_with_error "#{source_filename} does not exist" unless File.file? source_filename
 
+  index = source_filename.rindex(/\.markdown/)
+  exit_with_error "Filename does not contain '.markdown'" if index.nil?
+
+  title = source_filename[0...index]
+  new_filename = filename title
   contents = "#{header title}#{File.read(source_filename)}"
+
   File.write(new_filename, contents)
 end
 
