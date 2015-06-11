cd4c3ff11745f5ba8d6cfb149b4cba6385c110cd
Yatin Sarbalia <yatinsns@gmail.com>
2015-06-11 22:12:28 +0530

    Fix create_post script to append source filename as well

--- bin/create_post.rb
+++ bin/create_post.rb
@@ -2,18 +2,18 @@
 
 POSTS_DIR = ENV['POSTS_DIR']
 
-def contents
+def header title
   timestamp = `date "+%Y-%m-%d %H:%M:%S"`
   contents = "---\n" +
              "layout: post\n" +
-             "title: #{ARGV.join(' ')}\n" +
+             "title: #{title}\n" +
              "date: #{timestamp.chomp}\n" +
              "categories: \n" +
              "---\n\n"
 end
 
-def new_filename
-  new_filename = `echo $(date "+%Y-%m-%d")"-#{ARGV.join('-')}.markdown"`
+def filename title
+  new_filename = `echo $(date "+%Y-%m-%d")"-#{title.gsub(' ', '-')}.markdown"`
   "#{POSTS_DIR}/#{new_filename.chomp}"
 end
 
@@ -22,6 +22,12 @@ def main
     puts "Error: Environment variable 'POSTS_DIR' missing"
     exit 1
   end
+
+  source_filename = ARGV.first
+  title = ARGV.drop(1).join(' ')
+  new_filename = filename title
+
+  contents = "#{header title}#{File.read(source_filename)}"
   File.write(new_filename, contents)
 end
 
