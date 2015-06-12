e80d45494d7703ed4244b9451673fb12ccf882f9
Yatin Sarbalia <yatinsns@gmail.com>
2015-06-12 23:12:35 +0530

    Fix create_post to take input file as path also

--- bin/create_post.rb
+++ bin/create_post.rb
@@ -22,26 +22,48 @@ def exit_with_error text
   exit 1
 end
 
+def filename_from_path path
+  last_slash_index = path.rindex(/\//)
+  last_slash_index ? path[(last_slash_index+1)..-1] : path
+end
+
+def title_from_filename name
+  index = name.rindex(/\./)
+  name[0...index]
+end
+
 def main
   exit_with_error "Environment variable 'POSTS_DIR' missing" unless POSTS_DIR
 
-  source_filename = ARGV.first
-  exit_with_error "Markdown filename missing.\nUsage: create_post.rb <filename.markdown>" unless source_filename
-  exit_with_error "#{source_filename} does not exist" unless File.file? source_filename
+  source_file_path = ARGV.first
+  exit_with_error "Markdown filename missing.\nUsage: create_post.rb <filename.markdown>" unless source_file_path
+  exit_with_error "#{source_file_path} does not exist" unless File.file? source_file_path
 
-  index = source_filename.rindex(/\.md/)
+  index = source_file_path.rindex(/\.md/)
   exit_with_error "Filename does not contain '.md'" unless index
 
-  title = source_filename[0...index]
+  puts "Creating post..."  
+
+  source_filename = filename_from_path source_file_path
+  title = title_from_filename source_filename 
   new_filename = filename title
-  contents = "#{header title}#{File.read(source_filename)}"
+
+  contents = "#{header title}#{File.read(source_file_path)}"
 
   File.write(new_filename, contents)
+  puts "Post Created !!!"
   
+  puts "Adding your post to git repository..."
   `cd #{POSTS_DIR}`
   `git add #{new_filename}`
+
+  puts "Creating commit..."
   `git commit -m "Add post #{source_filename}"`
+
+  puts "Pushing commit..."
   `git push origin master`
+
+  puts "Done !!!"
 end
 
 main if __FILE__ == $0
