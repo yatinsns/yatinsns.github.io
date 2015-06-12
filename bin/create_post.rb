a45e72e2e96193fe9936b4a495ca90c8b3bfccdf
Yatin Sarbalia <yatinsns@gmail.com>
2015-06-13 00:12:54 +0530

    Make create_post more readable

--- bin/create_post.rb
+++ bin/create_post.rb
@@ -8,11 +8,11 @@ def header title
              "layout: post\n" +
              "title: #{title}\n" +
              "date: #{timestamp}\n" +
-             "categories: \n" +
+             "categories:\n" +
              "---\n\n"
 end
 
-def filename title
+def post_file_path_with_title title
   new_filename = `echo $(date "+%Y-%m-%d")"-#{title.gsub(' ', '-')}.md"`
   "#{POSTS_DIR}/#{new_filename.chomp}"
 end
@@ -27,41 +27,63 @@ def filename_from_path path
   last_slash_index ? path[(last_slash_index+1)..-1] : path
 end
 
-def title_from_filename name
-  index = name.rindex(/\./)
-  name[0...index]
+def title_from_filename filename
+  index = filename.rindex(/\./)
+  filename[0...index]
+end
+
+def title_from_file_path path
+  filename = filename_from_path path
+  title_from_filename filename
+end
+
+def contents_with_header_for_file_path path
+  title = title_from_file_path path
+  "#{header title}#{File.read(path)}"
+end
+
+def validate_input_file path
+  exit_with_error "Markdown filename missing.\nUsage: create_post.rb <filename.markdown>" unless path
+  exit_with_error "#{path} does not exist" unless File.file? path
+  exit_with_error "Filename does not contain '.md'" unless path.rindex(/\.md/)
+end
+
+def dest_file_path_for_source_file_path path
+  title = title_from_file_path path
+  post_file_path_with_title title
+end
+
+def copy_from_source_to_dest_with_header(source_file_path, dest_file_path)
+  contents = contents_with_header_for_file_path source_file_path
+  File.write(dest_file_path, contents)
+end
+
+def git_commit_with_file_path path
+  `cd #{POSTS_DIR}`
+  `git add #{path}`
+  `git commit -m "Add new post"`
+end
+
+def git_push
+  `git push origin master`
 end
 
 def main
   exit_with_error "Environment variable 'POSTS_DIR' missing" unless POSTS_DIR
 
   source_file_path = ARGV.first
-  exit_with_error "Markdown filename missing.\nUsage: create_post.rb <filename.markdown>" unless source_file_path
-  exit_with_error "#{source_file_path} does not exist" unless File.file? source_file_path
+  validate_input_file source_file_path
 
-  index = source_file_path.rindex(/\.md/)
-  exit_with_error "Filename does not contain '.md'" unless index
-
-  puts "Creating post..."  
-
-  source_filename = filename_from_path source_file_path
-  title = title_from_filename source_filename 
-  new_filename = filename title
-
-  contents = "#{header title}#{File.read(source_file_path)}"
-
-  File.write(new_filename, contents)
+  puts "Creating post..."
+  dest_file_path = dest_file_path_for_source_file_path source_file_path
+  copy_from_source_to_dest_with_header(source_file_path, dest_file_path)
   puts "Post Created !!!"
   
   puts "Adding your post to git repository..."
-  `cd #{POSTS_DIR}`
-  `git add #{new_filename}`
+  git_commit_with_file_path dest_file_path
 
-  puts "Creating commit..."
-  `git commit -m "Add post #{source_filename}"`
-
-  puts "Pushing commit..."
-  `git push origin master`
+  puts "Pushing the commit..."
+  git_push
 
   puts "Done !!!"
 end
